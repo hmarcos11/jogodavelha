1d5636784828ee6968703c37bdb6d1cc00b4090a
andrealmeidaa <andre.almeida@escolar.ifrn.edu.br>
2016-03-07 21:30:44 -0300

    Atualização Classe Tabuleiro
    Atualização da classe tabuleiro que implementa a regra do jogo.

--- JogoVelha/Tabuleiro.cs
+++ JogoVelha/Tabuleiro.cs
@@ -10,6 +10,7 @@ namespace JogoVelha
     {
         //Representa o tabuleiro do jogo da velha
         private char[,] matriz;
+        private int espacosLivres;//Controla quantas casas livres temos;
         private char jogadorDaVez; //Controla o jogador da Vez O ou X
         public enum Status {Empate =0,X=1,O=2,Continua=3}; // Enumeração utilizada para definir o resultado de cada movimento
         //Propriedade que retorna o jogador ativo
@@ -36,6 +37,7 @@ namespace JogoVelha
             for (int i = 0; i < 3; i++)
                 for (int j = 0; j < 3; j++)
                     matriz[i, j] = ' ';
+            espacosLivres = 9;
         }
 
         
@@ -44,6 +46,7 @@ namespace JogoVelha
             if (matriz[linha, coluna] == ' ')
             {
                 matriz[linha, coluna] = jogadorDaVez;
+                espacosLivres--;//Diminui o número de espaços livres
                 return true;
             }
             return false;   
@@ -76,10 +79,31 @@ namespace JogoVelha
             {
                 return VerificaJogador();
             }
-            else if (matriz[1, 0] == matriz[1, 1] && matriz[1, 1] == matriz[1, 2] && matriz[1, 0] == jogadorDaVez)
+            else if (matriz[0, 1] == matriz[1, 1] && matriz[1, 1] == matriz[2, 1] && matriz[0, 1] == jogadorDaVez)
             {
                 return VerificaJogador();
             }
+            else if (matriz[0, 2] == matriz[1, 2] && matriz[1, 2] == matriz[2, 2] && matriz[0, 2] == jogadorDaVez)
+            {
+                return VerificaJogador();
+            }//Verifica as diagonais
+            else if (matriz[0, 0] == matriz[1, 1] && matriz[1, 1] == matriz[2, 2] && matriz[0, 0] == jogadorDaVez)
+            {
+                return VerificaJogador();
+            }
+            else if(matriz[0,2]==matriz[1,1] && matriz[1,1]==matriz[2,0] && matriz[0,2]==jogadorDaVez)
+            {
+                return VerificaJogador();
+            }
+            else if(espacosLivres==0)
+            {
+                return Status.Empate;
+            }
+            else
+            {
+                return Status.Continua;
+            }
+
         }
 
 
