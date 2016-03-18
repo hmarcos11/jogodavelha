43a514d4b27a0d01d135b905d02e2e8b5d2e463f
hmarcos11 <marcosmarcone50@gmail.com>
2016-03-17 23:57:12 -0300

    redimensionando form jogo
    redimensionando form jogo

--- JogoVelha/FormJogo.Designer.cs
+++ JogoVelha/FormJogo.Designer.cs
@@ -34,8 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(553, 396);
             this.Name = "FormJogo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Jogo da Velha";
             this.ResumeLayout(false);
 
